ae5caa05fbe87ff0d3a8478a83cb8d295c79e8a5
EmmaVgn <emma.vigneron@outlook.fr>
2024-08-26 14:31:08 +0200

    add pages

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class AboutController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/about', name: 'about_index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-           'carousel' => true,
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
 }
--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class ServicesController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/services', name: 'services_index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-           'carousel' => true,
+        return $this->render('services/index.html.twig', [
+            'controller_name' => 'ServicesController',
         ]);
     }
 }
